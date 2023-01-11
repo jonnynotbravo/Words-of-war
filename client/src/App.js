6ddf82b201a9ba2d22a4b81a9447f9787883263a
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-01-11 11:17:12 -0500

    Finsihed frontend Signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
+import { Switch, Route } from "react-router-dom";
+
+import Signup from "./Components/Signup";
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Switch>
+      <Route exact path='/signup'>
+        <Signup />
+      </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,5 +1,71 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Signup = () => {
-  return <div></div>;
+  const history = useHistory();
+  const [data, setData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    password_confirmation: "",
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }).then(history.push("/"));
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={data.email}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={data.username}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={data.password}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input
+          type="password"
+          name="password_confirmation"
+          placeholder="Password Confirmation"
+          value={data.password_confirmation}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input type="submit" value="Signup" />
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
