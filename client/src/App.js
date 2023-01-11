65c6da1b73278a73cf7cfaf0e4bce7670c7d8ba4
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-01-11 11:39:25 -0500

    Finished frontend Signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import { Switch, Route } from "react-router-dom";
 
 import Signup from "./Components/Signup";
+import Login from './Components/Login'
 
 function App() {
   return <div className="App">
     <Switch>
       <Route exact path='/signup'>
         <Signup />
+      </Route>
+      
+      <Route exact path='/login'>
+        <Login />
       </Route>
     </Switch>
   </div>;
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,5 +1,52 @@
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+
 const Login = () => {
-  return <div></div>;
+  const history = useHistory();
+
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }).then(history.push("/"));
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={data.username}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={data.password}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
