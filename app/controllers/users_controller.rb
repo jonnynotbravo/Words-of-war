8342de08f2e1b077ab0b675b45e27b559a69104e
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-01-20 09:21:55 -0500

    Delete user backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,13 @@ class UsersController < ApplicationController
 
     end
 
+    def destroy 
+        user = User.find(session[:user_id])
+        user.destroy
+        head :no_content
+    end
+
+
     private 
 
     def user_params
