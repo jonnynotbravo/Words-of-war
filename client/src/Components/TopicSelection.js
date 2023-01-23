698f927f7c9e5f8b77fbbd70a2d408876137bc9d
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-01-22 23:52:32 -0500

    Topic Selection CSS part I

--- client/src/Components/TopicSelection.js
+++ client/src/Components/TopicSelection.js
@@ -6,20 +6,55 @@ const TopicSelection = ({ topics, setSelectedTopicId }) => {
 
   const topicsArray = topics.map((topic) => {
     return (
-      <li
-        id="topics"
-        key={topic.id}
-        onClick={() => {
-          setSelectedTopicId(topic.id);
-          history.push("/topic");
-        }}
-      >
-        {topic.title}
-      </li>
+      <>
+        <div className="cards">
+          <div className="card card-1">
+            <div className="card__icon">
+              <i className="fas fa-bolt"></i>
+            </div>
+            <p className="card__exit">
+              <i className="fas fa-times"></i>
+            </p>
+            <h2 className="card__title">{topic.title}</h2>
+            <p className="card__apply">
+              <a
+                className="card__link"
+                onClick={() => {
+                  setSelectedTopicId(topic.id);
+                  history.push("/topic");
+                }}
+              >
+                Enter Debate <i className="fas fa-arrow-right"></i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
     );
   });
 
-  return <div id="topicsContainer">{topicsArray}</div>;
+  return (
+    <div className="main-container">
+      {" "}
+      <div className="heading">
+        <h1 className="heading__title">Topics of the week</h1>
+      </div>
+      {topicsArray}
+    </div>
+  );
 };
 
 export default TopicSelection;
+
+{
+  /* <li
+id="topics"
+key={topic.id}
+onClick={() => {
+  setSelectedTopicId(topic.id);
+  history.push("/topic");
+}}
+>
+{topic.title}
+</li> */
+}
