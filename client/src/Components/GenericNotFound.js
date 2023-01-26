9ae95ef9bc02e10ea0f7930d8d9b375377ac82af
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-01-26 00:11:42 -0500

    Final

--- client/src/Components/GenericNotFound.js
+++ client/src/Components/GenericNotFound.js
@@ -1,8 +1,8 @@
 const GenericNotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <h1>Not Found</h1>
+    <div id='not-found'>
+      <h1>404 Not Found</h1>
+      
     </div>
   );
 };
